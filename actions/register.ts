dcc8e2f2aea45477e7af91adfd2730beccdfef11
jpaberzs <janis.paberzs18@gmail.com>
2024-06-30 12:10:13 +0300

    feat: Implemented creation of a new user

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields!' };
   }
 
-  return { success: 'Email sent!' };
+  const { email, name, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 11);
+
+  const existingEmail = await getUserByEmail(email);
+
+  if (existingEmail) {
+    return { error: 'Email already exists!' };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: Send verification token email
+
+  return { success: 'User created!' };
 };
